1d487a3c9b6b3fe0fbe599364584018254a2875d
saketh2 <89845550+saketh2@users.noreply.github.com>
2021-09-05 00:53:04 -0700

    Create assign1.sql

---
+++ assign1.sql
@@ -0,0 +1,4 @@
+# select cid from Campus where law=1;#
+# select name , rollno , age , math , sci , sports from Student where name="dave";#
+# select name , rollno from student where math>80 OR sci>80;#
+# select social from student ;
